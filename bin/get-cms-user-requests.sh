9845052c14e27762b65787d2d901527f73f9fdda
Trey Dockendorf <treydock@tamu.edu>
2015-10-05 15:27:41 -0500

    Improve get-cms-user-requests cron script * Remove hardcoded --pool value passed to get_user_requests.py - now value is set in primary INI config * JSON output is first written to .new file then moved to final location

--- bin/get-cms-user-requests.sh
+++ bin/get-cms-user-requests.sh
@@ -5,12 +5,14 @@ LOCK_FILE="/var/lock/subsys/get-cms-user-requests"
 
 if [ -f $CONFIG ]; then
   CONDOR_INSTALL=$(awk -F "=" '/^condor_install/ {print $2}' $CONFIG | tr -d ' ')
+  CONDOR_POOL=$(awk -F "=" '/^condor_pool/ {print $2}' $CONFIG | tr -d ' ')
   USER_REQUESTS_JSON=$(awk -F "=" '/^user_requests_json/ {print $2}' $CONFIG | tr -d ' ')
 
   [ -z "$CONDOR_INSTALL" ] && CONDOR_INSTALL="system"
   [ -z "$USER_REQUESTS_JSON" ] && USER_REQUESTS_JSON="/var/tmp/htcondor-pilot-job-router/cms_user_requests.json"
 else
   CONDOR_INSTALL="system"
+  CONDOR_POOL="vocms099.cern.ch,vocms0115.cern.ch"
   USER_REQUESTS_JSON="/var/tmp/htcondor-pilot-job-router/cms_user_requests.json"
 fi
 
@@ -21,7 +23,8 @@ fi
   [[ "x$CONDOR_INSTALL" == "xsystem" ]] || export PYTHONPATH=$CONDOR_INSTALL/lib/python:$PYTHONPATH
   [[ -d `dirname $USER_REQUESTS_JSON` ]] || mkdir `dirname $USER_REQUESTS_JSON`
 
-  /usr/libexec/htcondor-pilot-job-router/get_user_requests.py --pool vocms099.cern.ch,vocms0115.cern.ch 2>/dev/null 1>$USER_REQUESTS_JSON
+  /usr/libexec/htcondor-pilot-job-router/get_user_requests.py --pool $CONDOR_POOL 2>/dev/null 1>${USER_REQUESTS_JSON}.new
+  mv -f ${USER_REQUESTS_JSON}.new $USER_REQUESTS_JSON
 
 ) 200>$LOCK_FILE
 retval=$?
